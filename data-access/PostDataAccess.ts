51deefb7673a04b34fedeea643e66c021fb2890b
Guy Nahon <guy.nahon@gmail.com>
2024-01-10 17:15:23 +0200

    add getPosts to the PostDataAccess.ts

--- data-access/PostDataAccess.ts
+++ data-access/PostDataAccess.ts
@@ -1,6 +1,9 @@
+import { FilterAndPage } from "../models/FilterAndPage";
+
 export interface PostDataAccess <Post> {
     
     addPost(post: Post): Promise<void>;
+    getPosts(filterAndPageData: FilterAndPage): Promise<Array<Post>>;
     getPost(id: number): Promise<Post>;
     editPost(id: number, editDetails: Partial<Post>): Promise<void>;
     removePost(id: number): Promise<void>;
