9b4a8ce3560f2034ef380ae7a07583e63f3c310b
Guy Nahon <guy.nahon@gmail.com>
2024-01-24 15:43:24 +0200

    added src and ignore .env

--- dist/DataBasePassword.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.postgresPassword = void 0;
-exports.postgresPassword = "puehnui88";
--- tests/routes/PostRoutes.test.ts
+++ tests/routes/PostRoutes.test.ts
@@ -1,47 +1,48 @@
 describe('API Tests', () => {
 
-    let id = 102;
+    let id = 104;
+    const globalUrl = 'http://localhost:5000/post/';
 
 
-    describe('POST /post', () => {
-        it('should create a new post and return a 201 status', async () => {
-            const postData = {
-                "title": "my name is guy",
-                "body": "im 28 y/o (almost 29)",
-                "subject": "dailydigest",
-                "date": "24-04-95"
-            };
 
-            const response = await fetch('http://localhost:5000/post', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(postData),
-            });
+    it('should create a new post and return a 201 status', async () => {
+        const postData = {
+            "title": "my name is guy",
+            "body": "im 28 y/o (almost 29)",
+            "subject": "dailydigest",
+            "date": "24-04-95"
+        };
 
-            expect(response.status).toBe(201);
+        const response = await fetch(globalUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(postData),
         });
+
+        expect(response.status).toBe(201);
     });
 
-    describe('GET /post/:id', () => {
-        it('return the posts by the id', async () => {
-            const response = await fetch(`http://localhost:5000/post/${id}`);
-            const responseJson = await response.json();
-            
-            expect(response.status).toBe(200);
-            expect("my name is guy").toBe(responseJson.title);
-            
-        });
+
+
+    it('return the posts by the id', async () => {
+        const response = await fetch(globalUrl+id);
+        const responseJson = await response.json();
+
+
+        expect(response.status).toBe(200);
+        expect("my name is guy").toBe(responseJson.title);
+        
     });
 
-    describe('DELETE /post/:id', () => {
-        it('deletes post by id', async () => {
-            const response = await fetch(`http://localhost:5000/post/${id}/`, {method: 'DELETE'});
+    it('deletes post by id', async () => {
+        const response = await fetch(globalUrl+id, {method: 'DELETE'});
 
-            expect(response.status).toBe(200);
-            id+=1;
-        });
+
+        expect(response.status).toBe(200);
+        id+=1;
     });
+
 
 });
