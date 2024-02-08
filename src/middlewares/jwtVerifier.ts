7694131bb80aca3b327060e1ba26c140f5da32df
guynahon <guy.nahon@gmail.com>
2024-02-08 13:42:09 +0200

    added a jwtAdmin middleware that will prevent users that arent admins to crud posts

--- src/middlewares/jwtVerifier.ts
+++ src/middlewares/jwtVerifier.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 const jwt = require("jsonwebtoken");
 require('dotenv/config');
 
-export const jwtVerifier = (req: Request, res: Response, next: NextFunction) => {
+export const jwtUserVerifier = (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = req.headers.token;        
         const payload = jwt.verify(token, process.env.JWT_SECRET);
@@ -15,6 +15,34 @@ export const jwtVerifier = (req: Request, res: Response, next: NextFunction) => 
     } catch (err) {
         console.error((err as Error).message);
         res.status(401).send("Unauthorized");
+    }
+};
+
+export const jwtAdminVerifier = async (req: Request, res: Response, next: NextFunction) => {
+    
+    try {
+        const token = req.headers.token;
+        if (token === 'undefined') {
+            throw new Error('Unauthorized');
+        }
+
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        const timeStamp = payload.exp - ((Math.floor(Date.now() / 1000)));
+        if (timeStamp < 0) {
+            throw new Error('Expired Token');
+        }
+
+        const adminPromise = await fetch(`${process.env.SERVER_ROUTE}/users/${payload.id}`);
+        const admin = await adminPromise.json();
+        if (!admin.admin) {
+            throw new Error('Unauthorized');
+        }
+
+        next();
+
+    } catch (err) {
+        console.error((err as Error).message);
+        res.status(401).send((err as Error).message);
     }
 };
 
--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -2,16 +2,16 @@ import express, { Request, Response } from "express";
 import {PostController} from "../controllers/PostController";
 import { PostServices } from "../services/PostServices";
 import { PostDataAccessSQL } from "../data-access/PostDataAccessSQL";
-import { jwtVerifier } from "../middlewares/jwtVerifier";
+import { jwtUserVerifier, jwtAdminVerifier } from "../middlewares/jwtVerifier";
 
 const router = express.Router();
 const postController = new PostController(new PostServices(new PostDataAccessSQL()));
 
-router.post('/', async (req: Request, res: Response) => await postController.addPost(req,res));
+router.post('/',jwtAdminVerifier, async (req: Request, res: Response) => await postController.addPost(req,res));
 router.get('/', async (req: Request, res: Response) => await postController.getPosts(req, res));
-router.delete('/clear', jwtVerifier, async (req: Request, res: Response) => await postController.clearPosts(req,res));
+router.delete('/clear', jwtAdminVerifier, async (req: Request, res: Response) => await postController.clearPosts(req,res));
 router.get('/:id', async (req: Request, res: Response) => await postController.getPost(req,res));
-router.put('/:id', jwtVerifier, async (req: Request, res: Response) => await postController.editPost(req,res));
-router.delete('/:id', jwtVerifier, async (req: Request, res: Response) => await postController.removePost(req,res));
+router.put('/:id', jwtAdminVerifier, async (req: Request, res: Response) => await postController.editPost(req,res));
+router.delete('/:id', jwtAdminVerifier, async (req: Request, res: Response) => await postController.removePost(req,res));
 
 export default router
